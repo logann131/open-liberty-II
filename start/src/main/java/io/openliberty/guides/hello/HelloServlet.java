a8f0310e820459d114dac23f1851e42316e9ee03
lgad31vn <lgad31vn@gmail.com>
2022-01-24 21:16:34 -0500

    added new readme

--- start/src/main/java/io/openliberty/guides/hello/HelloServlet.java
+++ start/src/main/java/io/openliberty/guides/hello/HelloServlet.java
@@ -29,7 +29,7 @@ public class HelloServlet extends HttpServlet {
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
         throws ServletException, IOException {
-        response.getWriter().append("Hello! How are you?\n");
+        response.getWriter().append("Hello! How are you? Smile!!\n");
     }
 
     /**
